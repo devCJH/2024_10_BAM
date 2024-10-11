37867594a1d9cd158d7a9b10e1231b8ef4ee3c03
devCJH <http-@naver.com>
2024-10-11 12:45:53 +0900

    article list search

--- src/com/koreaIT/BAM/Main.java
+++ src/com/koreaIT/BAM/Main.java
@@ -46,17 +46,41 @@ public class Main {
 				
 				System.out.printf("%d번 게시물이 생성되었습니다\n", lastArticleId);
 				
-			} else if (cmd.equals("article list")) {
+			} else if (cmd.startsWith("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시물이 존재하지 않습니다");
 					continue;
 				}
+
+				List<Article> printArticles = articles;
+				
+				String searchKeyword = cmd.substring("article list".length()).trim();
+				
+				
+				if (searchKeyword.length() > 0) {
+
+					System.out.println("검색어 : " + searchKeyword);
+					
+					printArticles = new ArrayList<>();
+					
+					for (Article article : articles) {
+						if (article.getTitle().contains(searchKeyword)) {
+							printArticles.add(article);
+						}
+					}
+					
+					if (printArticles.size() == 0) {
+						System.out.println("검색결과가 없습니다");
+						continue;
+					}
+				}
 				
 				System.out.println("번호	|	제목	|	작성일	");
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
+				for (int i = printArticles.size() - 1; i >= 0; i--) {
+					Article article = printArticles.get(i);
 					System.out.printf("%d	|	%s	|	%s	\n", article.getId(), article.getTitle(), article.getRegDate());
 				}
+				
 			} else if (cmd.startsWith("article detail ")) {
 				int id = getCmdNum(cmd);
 				
